9f5977235396713b259100e45a00ed933f0d36c8
Katia Aresti <karesti@redhat.com>
2025-05-14 11:14:39 +0200

    Fix serialization for Spring-Boot

--- inmemory-catalogue-spring-boot/src/main/java/org/infinispan/inmemory/schema/RetailProductValue.java
+++ inmemory-catalogue-spring-boot/src/main/java/org/infinispan/inmemory/schema/RetailProductValue.java
@@ -2,12 +2,13 @@ package org.infinispan.inmemory.schema;
 
 import org.infinispan.api.annotations.indexing.Basic;
 import org.infinispan.api.annotations.indexing.Indexed;
+import org.infinispan.api.annotations.indexing.Text;
 import org.infinispan.protostream.annotations.Proto;
 
 @Proto
 @Indexed
 public record RetailProductValue(@Basic String code,
-                                 @Basic String name,
+                                 @Text String name,
                                  @Basic Double price,
                                  @Basic Integer stock) {
 
