509fc8d4005553c0f34d536c19e7e0c11ddc4c5e
Katia Aresti <karesti@redhat.com>
2025-05-14 10:55:53 +0200

    Final fixes for migration

--- inmemory-catalogue/src/main/java/org/infinispan/inmemory/schema/PurchasedProductValue.java
+++ inmemory-catalogue/src/main/java/org/infinispan/inmemory/schema/PurchasedProductValue.java
@@ -2,14 +2,14 @@ package org.infinispan.inmemory.schema;
 
 import org.infinispan.api.annotations.indexing.Basic;
 import org.infinispan.api.annotations.indexing.Indexed;
-import org.infinispan.api.annotations.indexing.Keyword;
+import org.infinispan.api.annotations.indexing.Text;
 import org.infinispan.protostream.annotations.Proto;
 
 @Proto
 @Indexed
-public record PurchasedProductValue(@Keyword
+public record PurchasedProductValue(@Text
                                     String name,
-                                    @Basic
+                                    @Text
                                     String country) {
 
 }
--- inmemory-catalogue/src/main/java/org/infinispan/inmemory/schema/RetailProductValue.java
+++ inmemory-catalogue/src/main/java/org/infinispan/inmemory/schema/RetailProductValue.java
@@ -2,13 +2,13 @@ package org.infinispan.inmemory.schema;
 
 import org.infinispan.api.annotations.indexing.Basic;
 import org.infinispan.api.annotations.indexing.Indexed;
-import org.infinispan.api.annotations.indexing.Keyword;
+import org.infinispan.api.annotations.indexing.Text;
 import org.infinispan.protostream.annotations.Proto;
 
 @Proto
 @Indexed
 public record RetailProductValue(String code,
-                                 @Keyword
+                                 @Text
                                  String name,
                                  @Basic
                                  Double price,
